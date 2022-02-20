d5ebe6f2ffdf75ceffb2e7dfbfa833bfdc544c50
Robert Freeman <robbie.freeman92@icloud.com>
2022-02-20 14:34:22 -0500

    Added seed files and a service for categories

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,18 @@
-const path = require("path");
 require("dotenv").config();
-const { DATABASE_URL} = process.env;
+const { DATABASE_URL } = process.env;
+const path = require("path");
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: DATABASE_URL,
+    client: "postgresql",
+    connection: "postgres://ehzsaclv:zXo2x3NwrUNZzSYz5GuyLI5tUV640O5B@jelani.db.elephantsql.com/ehzsaclv",
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
     }
   },
 };
+
+
