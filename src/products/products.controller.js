5481e71c39f8c5eea9913c116642fbdf254c174e
Robert Freeman <robbie.freeman92@icloud.com>
2022-02-20 15:47:51 -0500

    Added supplier services and middleware

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -1,7 +1,8 @@
 const productsService = require("./products.service");
 
 function read(req, res, next) {
-  res.json({ data: { product_title: "some product title" } });
+  const { product: data } = res.locals;
+  res.json({ data });
 }
 
 function list(req, res, next) {
@@ -11,7 +12,20 @@ function list(req, res, next) {
     .catch(next);
 }
 
+function productExists(req, res, next) {
+  productsService
+    .read(req.params.productId)
+    .then((product) => {
+      if (product) {
+        res.locals.product = product;
+        return next();
+      }
+      next({ status: 404, message: `Product cannot be found.` });
+    })
+    .catch(next);
+  }
+
 module.exports = {
-  read: [read],
+  read: [productExists, read],
   list: [list],
 };
--- src/products/products.service.js
+++ src/products/products.service.js
@@ -4,6 +4,11 @@ function list() {
   return knex("products").select("*");
 }
 
+function read(productId) {
+  return knex("products").select("*").where({ product_id: productId }).first();
+}
+
 module.exports = {
   list,
+  read,
 };
