b60715d98c6afe0227cb67332f12dde1cbb6d795
Robert Freeman <robbie.freeman92@icloud.com>
2022-02-19 19:13:22 -0500

    Created Categories Table

--- src/db/migrations/20220220001110_createCategoriesTable.js
+++ src/db/migrations/20220220001110_createCategoriesTable.js
@@ -3,7 +3,12 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("categories", (table) => {
+    table.increments("category_id").primary();
+    table.string("category_name");
+    table.text("category_description");
+    table.timestamps(true, true);
+  });
 };
 
 /**
@@ -11,5 +16,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("categories");
 };
